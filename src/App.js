2648cd0fff7fbbd98c550f3d25fd883b52b30b30
JetCyC <670182161@qq.com>
2020-07-27 21:37:05 +0800

    fix

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Let's Go!Baby!     Niu,Niu,Niu!!!!!!!!!!
+          Let's Go!Baby!      Niu,Niu,Niu!!!!!!!!!!
         </p>
         <a
           className="App-link"
